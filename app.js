6df21f6136d659b4454e93ab2efbc8721d66908a
karthikeyan-u <karthikeyan_u@outlook.com>
2018-11-02 10:44:57 +0530

    added routes

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ app.set('view engine','ejs');
 app.get('/',function(req,res){
     res.render('home');
 });
-
+app.get('/profile',function(req,res){
+    res.render('profile');
+});
+app.get('/profile/:name',function(req,res){
+    var name = req.params.name;
+    res.send('This is the profile of '+ name);
+});
+app.post('/profile',function(req,res){
+    res.send('Post route reached');
+});
 //LISTEN//
 app.listen(process.env.PORT||3000,function(){
     console.log('Server Started and Listening on Port : 3000');
